4f7fd826a531bf162d0f824a25b49b9ddcb0dff7
hanchung <ghoo1125@gmail.com>
2018-05-22 14:45:15 +0800

    Use PlacesService to find nearby restaurant.

--- js/map.js
+++ js/map.js
@@ -9,22 +9,57 @@ function initMap(curPos) {
   return map
 }
 
+function createMarker(map, infoWindow, pos, name) {
+  let marker = new google.maps.Marker({
+    map: map,
+    position: pos,
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(name);
+    infoWindow.open(map, this);
+  });
+}
+
+function createPath(map, infoWindow, places) {
+  createMarker(map, infoWindow, places[0].geometry.location, places[0].name);
+}
+
+function searchRestaurant(map, infoWindow, pos) {
+  let service = new google.maps.places.PlacesService(map);
+
+  let request = {
+    location: pos,
+    radius: '500', /* maximum 50,000 meter */
+    types: ['restaurant'],
+  };
+  service.nearbySearch(request, (places, status) => {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createPath(map, infoWindow, places);
+    }
+  });
+}
+
 function main() {
+  let btn = document.getElementById('btn-go');
   let map = initMap();
+  let infoWindow = new google.maps.InfoWindow({map: map});
+  let pos;
 
-  // Set to current position
+  // Set to current position and register btn event
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(curPos) {
-      let infoWindow = new google.maps.InfoWindow({map: map});
-      let pos = {lat: curPos.coords.latitude, lng: curPos.coords.longitude};
-      infoWindow.setPosition(pos);
-      infoWindow.close();
-      map.setCenter(pos);
-      map.setZoom(17);
-
-      let marker = new google.maps.Marker({
-        position: pos,
-        map: map,
+    new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(function(curPos) {
+        pos = {lat: curPos.coords.latitude, lng: curPos.coords.longitude};
+        map.setCenter(pos);
+        map.setZoom(15);
+        let name = ;
+        createMarker(map, infoWindow, pos, '你的位置');
+        resolve();
+      });
+    }).then(() => {
+      btn.addEventListener('click', function() {
+        searchRestaurant(map, infoWindow, pos);
       });
     });
   }
